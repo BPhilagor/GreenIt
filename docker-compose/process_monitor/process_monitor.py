a5943595f95fdc5aea7a6024698d4c53a5cf03b7
BPhilagor <benjamin.philagor@gmail.com>
2024-02-03 19:53:27 +0100

    Last version + bad_hello

--- docker-compose/process_monitor/process_monitor.py
+++ docker-compose/process_monitor/process_monitor.py
@@ -2,18 +2,16 @@ from flask import Flask, Response
 import subprocess
 
 app = Flask(__name__)
+program_name = "bad_hello"
 
 def get_process_info(process_name):
     pid = subprocess.getoutput(f"pgrep -f {process_name}").splitlines()
     commands = list(map(lambda p: f"ps -p {p} -o %cpu,%mem", pid))
-    print("Process nbr:", len(commands))
-    print(commands)
     if len(commands) > 0:
         cpu_usage = 0.0
         mem_usage = 0.0
         for command in commands:
             stats = subprocess.getoutput(command).splitlines()
-            print(stats)
             if len(stats) == 2:
                 stats_tab = stats[1].split()
                 cpu_usage += float(stats_tab[0])
@@ -23,15 +21,15 @@ def get_process_info(process_name):
 
 @app.route('/metrics')
 def metrics():
-    cpu_usage, mem_usage = get_process_info("firefox")
+    cpu_usage, mem_usage = get_process_info(program_name)
     if cpu_usage != None and mem_usage != None:
         metrics = [
             f"# HELP process_cpu_usage CPU usage of the process",
             f"# TYPE process_cpu_usage gauge",
-            f"process_cpu_usage{{process=\"firefox\"}} {cpu_usage}",
+            f"process_cpu_usage{{process=\"{program_name}\"}} {cpu_usage}",
             f"# HELP process_mem_usage Memory usage of the process",
             f"# TYPE process_mem_usage gauge",
-            f"process_mem_usage{{process=\"firefox\"}} {mem_usage}",
+            f"process_mem_usage{{process=\"{program_name}\"}} {mem_usage}",
         ]
         return Response('\n'.join(metrics), mimetype='text/plain')
     return Response("Error: Process not found", status=404, mimetype='text/plain')
--- programs/time_inefficient_hello_world.c
+++ programs/time_inefficient_hello_world.c
@@ -4,11 +4,12 @@
 int main() {
     // Loop to introduce delay
     for (long i = 0; i < 1000000; i++) {
-        for (long j = 0; i < 1000000; j++) {
+        for (long j = 0; j < 10000000000; j++) {
             long test = i * j;
             (void) test;
         }
         printf("%ld\n", i);
+        sleep(3);
     }
 
     return 0;
